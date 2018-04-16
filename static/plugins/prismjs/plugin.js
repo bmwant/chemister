d05d0a2818ea9ac37b9d3d7ffa96987ffe6e7eab
Misha Behersky <bmwant@gmail.com>
2018-04-16 18:01:37 +0300

    Resolve loading issue with requirejs :star2:

--- static/plugins/prismjs/plugin.js
+++ static/plugins/prismjs/plugin.js
@@ -1,18 +1,18 @@
 require.config({
     paths: {
-        'prismjs': 'assets/plugins/prismjs/js/prism.pack',
+        'prismjs': 'static/plugins/prismjs/js/prism.pack',
     },
-	shim: {
-		prism: {
-			exports: "Prism"
-		}
-	}
+    shim: {
+        prism: {
+            exports: "Prism"
+        }
+    }
 });
 
 require(['prismjs', 'jquery'], function(prismjs, $){
     $(document).ready(function(){
         // $('[class^="language-"]').each(function(i, block) {
-	     //    Prism.highlightElement(block);
+         //    Prism.highlightElement(block);
         // });
     });
 });
