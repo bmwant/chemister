5b1ffe4b63a6406f9b60a225d22ba296bfcee9b6
Misha Behersky <bmwant@gmail.com>
2018-04-22 11:51:15 +0200

    Check on test env when initializing db :smirk:

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,1 +1,4 @@
 #!/usr/bin/env bash
+
+
+# todo: add colored print helper functions
--- scripts/run_sql.sh
+++ scripts/run_sql.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -6,6 +7,7 @@ HOST=${PG_HOST:="local-postgres"}
 DBNAME=${PG_DATABSE:="chemister"}
 USER=${PG_USER:="che"}
 PASSWORD=${PG_PASSWORD:="guevara"}
+TEST=${TEST:=true}
 
 echo "::running base sql"
 psql -U postgres -h "${HOST}" -f "${DIR}/sql/000_drop_everything.sql"
@@ -17,7 +19,10 @@ echo "::running additional sql"
 
 psql -U "${USER}" -h "${HOST}" -d "${DBNAME}" -f "${DIR}/sql/002_create_tables.sql"
 
-echo "::inserting test data"
-psql -U "${USER}" -h "${HOST}" -d "${DBNAME}" -f "${DIR}/sql/003_insert_base_data.sql"
-# todo: add flag for test database
-# todo: fail on errors
+
+if [ -z "${TEST}" ]; then
+  echo "::production env, do nothing"
+else
+  echo "::inserting test data"
+  psql -U "${USER}" -h "${HOST}" -d "${DBNAME}" -f "${DIR}/sql/003_insert_base_data.sql"
+fi
