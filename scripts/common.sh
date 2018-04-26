935ae520dbca0c6b62968c7936737448f86250e4
Misha Behersky <bmwant@gmail.com>
2018-04-26 12:19:19 +0300

    Update scripts :baggage_claim:

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
+set -e
 
 
-# todo: add colored print helper functions
+function _note {
+  echo -e "\033[0;32m========================"
+  echo $1
+  echo -e "========================\033[0m"
+}
+
+function _info {
+  echo -e "\033[0;33m========================"
+  echo $1
+  echo -e "========================\033[0m"
+}
+
+function _warn {
+  echo -e "\033[0;31m========================"
+  echo $1
+  echo -e "========================\033[0m"
+}
--- scripts/run_sql.sh
+++ scripts/run_sql.sh
@@ -2,8 +2,8 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${DIR}/common.sh"
 
-# todo: out variables here
 
 # Fallback to local values when not defined
 HOST=${PG_HOST:="local-postgres"}
@@ -11,24 +11,38 @@ DBNAME=${PG_DATABASE:="chemister"}
 USER=${PG_USER:="che"}
 PASSWORD=${PG_PASSWORD:="guevara"}
 POSTGRES_USER_PASS=${POSTGRES_USER_PASS:="postgres"}
-TEST=${TEST:=true}
+if [ -z ${TEST+x} ]; then
+ TEST=true
+fi
 
+echo "HOST ${HOST}"
+echo "DBNAME ${DBNAME}"
+echo "USER ${USER}"
+echo "PASSWORD ${PASSWORD}"
+echo "POSTGRES_USER_PASS ${POSTGRES_USER_PASS}"
+echo "TEST ${TEST}"
+if [ -z "${TEST}" ]; then
+  _info "|| production env, not test"
+fi
+exit
 # PGPASSWORD should be exported to the environment
 export PGPASSWORD="${POSTGRES_USER_PASS}"
-echo "::running base sql"
+_info "|| running base sql"
 psql -U postgres -h "${HOST}" -f "${DIR}/sql/000_drop_everything.sql"
 psql -U postgres -h "${HOST}" -f "${DIR}/sql/001_init_database.sql"
 
 # Update password to correspond with the new user
 export PGPASSWORD="${PASSWORD}"
-echo "::running additional sql"
+info "|| running additional sql"
 
 psql -U "${USER}" -h "${HOST}" -d "${DBNAME}" -f "${DIR}/sql/002_create_tables.sql"
 
 
 if [ -z "${TEST}" ]; then
-  echo "::production env, do nothing"
+  _info "|| production env, do nothing"
 else
-  echo "::inserting test data"
+  _info "|| inserting test data"
   psql -U "${USER}" -h "${HOST}" -d "${DBNAME}" -f "${DIR}/sql/003_insert_base_data.sql"
 fi
+
+_note "|| done!"
