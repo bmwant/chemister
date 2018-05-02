36963186d9a8912ea7dba1346e32d556473eea42
Misha Behersky <bmwant@gmail.com>
2018-05-02 16:27:47 +0300

    Customize chart for monthly profit :dizzy_face:

--- crawler/models/charts.py
+++ crawler/models/charts.py
@@ -27,7 +27,26 @@ async def get_profit_last_month(conn):
         elif bid.bid_type == BidType.OUT.value:
             data[day_key] += bid.amount * bid.rate
 
-    return [{'date': key, 'value': value} for key, value in data.items()]
+    # return [{'date': key, 'value': value} for key, value in data.items()] * 30
+    return [
+        {'date': '01/05/18', 'value': 500},
+        {'date': '02/05/18', 'value': 600},
+        {'date': '03/05/18', 'value': 700},
+        {'date': '04/05/18', 'value': 300},
+        {'date': '05/05/18', 'value': -100},
+        {'date': '06/05/18', 'value': -500},
+        {'date': '07/05/18', 'value': 120},
+        {'date': '08/05/18', 'value': 400},
+        {'date': '09/05/18', 'value': 1200},
+        {'date': '10/05/18', 'value': 500},
+        {'date': '11/05/18', 'value': 500},
+        {'date': '12/05/18', 'value': 700},
+        {'date': '13/05/18', 'value': 710},
+        {'date': '14/05/18', 'value': 800},
+        {'date': '15/05/18', 'value': 500},
+        {'date': '16/05/18', 'value': 505},
+        {'date': '17/05/18', 'value': 10},
+    ]
 
 
 async def get_closed_bids_for_period(
