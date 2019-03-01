7a0b9259aff37be751f6c8a18c10385dce82e486
Misha Behersky <bmwant@gmail.com>
2019-03-01 15:00:44 +0200

    Load data to train NN :oden:

--- notebooks/cardrive/carmax_nn.py
+++ notebooks/cardrive/carmax_nn.py
@@ -11,32 +11,46 @@ import pandas as pd
 
 
 def get_data():
-    pass
+    num_actions = 7
+    df = pd.read_csv('play_data.csv', header=None)
+    X = df.loc[:,0:4].to_numpy()  # select first 5 rows
+    actions = df.iloc[:,-1]  # select last row
+    # convert actions to softmax format
+    y = np.zeros((len(actions), num_actions))
+    y[np.arange(len(actions)), actions] = 1
+    return X, y
 
 
 def main():
-    # X, y = get_data()
-    X = np.array([
-        [1, 2, 3, 4, 5],
-        [2, 4, 6, 8, 10],
-        [3, 6, 9, 12, 18],
-        [4, 8, 12, 16, 20],
-    ])
-
-    y = np.array([
-        [1, 0, 0, 0, 0, 0, 1],
-        [4, 0, 0, 0, 0, 0, 1],
-        [9, 0, 0, 0, 0, 0, 1],
-        [16, 0, 0, 0, 0, 0, 1],
-    ])
-    tf.reset_default_graph()
+    """
+    Supervised learning. Use best results from random agent.
+    """
+    X, y = get_data()
+    # X = np.array([
+    #     [1, 2, 3, 4, 5],
+    #     [2, 4, 6, 8, 10],
+    #     [3, 6, 9, 12, 18],
+    #     [4, 8, 12, 16, 20],
+    # ])
+    #
+    # y = np.array([
+    #     [1, 0, 0, 0, 0, 0, 1],
+    #     [4, 0, 0, 0, 0, 0, 1],
+    #     [9, 0, 0, 0, 0, 0, 1],
+    #     [16, 0, 0, 0, 0, 0, 1],
+    # ])
+    # tf.reset_default_graph()
     tflearn.init_graph(num_cores=8)
 
     net = tflearn.input_data(shape=[None, 5])
     net = tflearn.fully_connected(net, 6)
     # net = tflearn.dropout(net, 0.5)
     net = tflearn.fully_connected(net, 7, activation='softmax')
-    net = tflearn.regression(net, optimizer='adam', loss='categorical_crossentropy')
+    net = tflearn.regression(
+        net,
+        optimizer='adam',
+        loss='categorical_crossentropy',
+    )
 
     model = tflearn.DNN(net)
     model.fit(X, y, n_epoch=100)
@@ -45,7 +59,7 @@ def main():
         [5, 10, 14, 19, 26],
         [1, 2, 3, 4, 5],
         [6, 12, 18, 24, 30],
-    
+
     ]))
 
 
